af1b5ee4f1bd4fd73dff8e98b519d5eb9806ec77
lautaroh2394 <lautarohaase_23@hotmail.com>
2020-11-29 16:19:04 -0300

    corrijo respuestas de los métodos: ahora devuelven un json correcto

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,6 +1,6 @@
 import os
 import json
-from flask import Flask, redirect, url_for, request
+from flask import Flask, request, jsonify, make_response
 from flaskr.DataManager import DataManager
 from flaskr.SessionManager import SessionManager
 
@@ -13,39 +13,44 @@ def create_app(test_config=None):
     SM = SessionManager.get_instance()
 
     def logueado(request):
-        return SM.usuario_logueado(
+        return jsonify(SM.usuario_logueado(
             request.form["usuario"],
             request.form["session_key"]
-            )
+            ))
 
     @app.route('/login', methods=["POST"])
     def login():
+        if "usuario" not in request.form or "password" not in request.form:
+            return jsonify(SM.LOGIN_ERROR)
         logged = SM.login_usuario(
             request.form["usuario"],
             request.form["password"]
             )
         if logged == False:
-            return SessionManager.LOGIN_ERROR
+            return jsonify(SessionManager.LOGIN_ERROR)
         
-        return str({
+        return jsonify({
             "exito" : True,
             "session_key": logged
         })
     
     @app.route('/signup', methods=['POST'])
     def signup():
+        if "usuario" not in request.form or "password" not in request.form:
+            return jsonify(SM.LOGIN_ERROR)
+
         rdo = DMI.nuevo_usuario(
             request.form["usuario"],
             request.form["password"]
             )
-        return str({
+        return jsonify({
             "exito": rdo
         })
 
     @app.route("/encuesta", methods=['POST'])
     def encuesta():
         if not logueado(request): 
-            return SessionManager.USUARIO_NO_LOGUEADO
+            return jsonify(SessionManager.USUARIO_NO_LOGUEADO)
 
         preguntas = json.loads(request.form["preguntas"])
         preguntas_id = []
@@ -59,7 +64,7 @@ def create_app(test_config=None):
             etiquetas = json.loads(request.form["etiquetas"])
 
         id_encuesta = DMI.nueva_encuesta(preguntas_id, etiquetas)
-        return str({
+        return jsonify({
                 "exito": True,
                 "id_encuesta": id_encuesta
             })
@@ -67,32 +72,32 @@ def create_app(test_config=None):
     @app.route("/encuesta/<encuesta_id>", methods=['GET'])
     def get_encuesta(encuesta_id=None):
         if encuesta_id is None:
-            return str({
+            return jsonify({
                 "exito" : False,
                 "mensaje" : "Debe indicar un id de encuesta"
             })
         
-        return str({
+        return jsonify({
             "exito" : True,
             "encuesta" : DMI.encuesta(encuesta_id)
             })
     
     @app.route('/listar?etiquetas=<etiquetas>', methods=['POST'])
     def listar_etiquetas(etiquetas=[]):
-        if not logueado(request): return SessionManager.USUARIO_NO_LOGUEADO
+        if not logueado(request): return jsonify(SessionManager.USUARIO_NO_LOGUEADO)
         
-        return str(DMI.encuestas_segun_etiquetas(json.loads(etiquetas)))
+        return jsonify(DMI.encuestas_segun_etiquetas(json.loads(etiquetas)))
     
     @app.route('/listar', methods=['POST'])
     def listar():
         if not logueado(request):
-            return SessionManager.USUARIO_NO_LOGUEADO
-        return str(DMI.encuestas_segun_etiquetas([]))
+            return jsonify(SessionManager.USUARIO_NO_LOGUEADO)
+        return jsonify(DMI.encuestas_segun_etiquetas([]))
     
     @app.route('/verificar/encuesta_id=<encuesta_id>&respuestas=<respuestas_dadas>', methods=['GET'])
     def verificar_encuesta(encuesta_id=None, respuestas_dadas=[]):
         if encuesta_id is None or respuestas_dadas is None:
-            return str({
+            return jsonify({
                 "exito" : False,
                 "mensaje" : "Debe indicar tanto el id de la encuesta como las respuestas"
             })
@@ -100,7 +105,7 @@ def create_app(test_config=None):
         respuestas_dadas = json.loads(respuestas_dadas)
         resultados = DMI.verificar_encuesta(encuesta_id, respuestas_dadas)
         resultados["exito"] = True
-        return str(resultados)
+        return jsonify(resultados)
 
     @app.route('/listar_dm')
     def listarDM():
@@ -108,7 +113,7 @@ def create_app(test_config=None):
         usuarios = [{"usuario_id": u.id} for u in DMI.usuarios]
         logueados = [{"usuario_id": u.id, "s_k": u.session_key} for u in SM.usuarios_logueados]
         encuestas = [DMI.encuesta(e.id) for e in DMI.encuestas]
-        return str({
+        return jsonify({
             "usuarios" : usuarios,
             "logueados": logueados,
             "encuestas" : encuestas
